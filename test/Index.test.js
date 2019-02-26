488f8c75cd45a7ae318997cd86e2268943bba94a
Todd Mantell <tmantell99@yahoo.com>
2019-02-26 11:21:02 -0600

    added more tests for IndexPage

--- test/Index.test.js
+++ test/Index.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Enzyme, { mount } from "enzyme";
+import Enzyme, { mount, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Index from "../pages";
 
@@ -14,5 +14,33 @@ describe("IndexPage", () => {
     expect(mountedIndex.find("button").length).toEqual(1);
     expect(mountedIndex.find("Room").length).toEqual(4);
     expect(mountedIndex.find("GuestDropdown").length).toEqual(8);
+  });
+
+  it("should handle a disable event", () => {
+    const IndexInstance = shallow(<Index />).instance();
+    const enableDisableSpy = jest.spyOn(IndexInstance, "enableOrDisable");
+    const disableSpy = jest.spyOn(IndexInstance, "disableThisAndLaterRooms");
+    IndexInstance.updateRoom(1, { type: "checkbox", checked: false });
+
+    expect(enableDisableSpy).toHaveBeenCalledTimes(1);
+    expect(disableSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should handle an enable event", () => {
+    const IndexInstance = shallow(<Index />).instance();
+    const enableSpy = jest.spyOn(IndexInstance, "enablePreviousRooms");
+
+    IndexInstance.updateRoom(1, { type: "checkbox", checked: true });
+
+    expect(enableSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should handle an update to the number of guests", () => {
+    const IndexInstance = shallow(<Index />).instance();
+    const enableSpy = jest.spyOn(IndexInstance, "handleGuestAmounts");
+
+    IndexInstance.updateRoom(1, { type: "select" });
+
+    expect(enableSpy).toHaveBeenCalledTimes(1);
   });
 });
